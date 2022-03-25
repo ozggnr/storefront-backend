1bb94ecaea3b82691942e0ac24a29a83367e7e39
ozggnr <ozgecaylioglu@gmail.com>
2022-03-24 22:34:07 -0400

    changed localhost

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import express, { Request, Response } from "express"
 import bodyParser from "body-parser"
 
 const app: express.Application = express()
-const address: string = '0.0.0.0:5000'
+const address: string = '0.0.0.0:3000'
 app.use(bodyParser.json)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello!')
 })
-app.listen(5000, () => console.log('app is ready:)'))
+app.listen(address, () => console.log('app is ready:)'))
