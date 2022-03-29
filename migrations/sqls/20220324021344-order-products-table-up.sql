a3f2ec2af8b90542fdd2159d294554e901155bd8
ozggnr <ozgecaylioglu@gmail.com>
2022-03-28 22:36:02 -0400

    updated db tables

--- migrations/sqls/20220324021344-order-products-table-up.sql
+++ migrations/sqls/20220324021344-order-products-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_products (
     id SERIAL PRIMARY KEY NOT NULL,
-    order_id INTEGER REFERENCES orders(id),
-    product_id INTEGER REFERENCES products(id),
+    orderId INTEGER REFERENCES orders(id),
+    productId INTEGER REFERENCES products(id),
     quantity INTEGER
 );
