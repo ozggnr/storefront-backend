dadf323db976a6cc48ed880da218fb2ccc13e90b
ozggnr <ozgecaylioglu@gmail.com>
2022-03-31 23:59:25 -0400

    added token to create product

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express"
-import { ProductDB } from '../models/products-model'
+import { ProductDB, Product } from '../models/products-model'
+import jwt from 'jsonwebtoken'
 
 const products = new ProductDB()
+const secret = process.env.TOKEN_SECRET || ''
 
 //Connection to the model
 const index = async (req: Request, res: Response) => {
@@ -13,8 +15,36 @@ const show = async (req: Request, res: Response) => {
     const product = await products.show(req.params.id)
     res.json(product)
 }
+const create = async (req: Request, res: Response) => {
+    const product: Product = {
+        id: req.body.id as Number,
+        name: req.body.name as string,
+        price: req.body.price as Number
+    }
+    try {
+        const newProduct = await products.create(product)
+        const secret = process.env.TOKEN_SECRET || ''
+        var token = jwt.sign({user: newProduct}, secret)
+        res.json(token)
+    } catch (error) {
+        res.send('New product can not be added')
+
+    }
+}
+const verifyAuthToken = async (req: Request, res: Response, next: Function) => {
+    try {
+        const authHeader = req.headers.authorization || ''
+        const token = authHeader.split(' ')[1]
+        jwt.verify(token, secret)
+        next()
+    } catch (error) {
+        res.status(401)
+        res.send(`Invalid token`)
+    }
+}
 const productRoutes = (app: express.Application) => {
     app.get('/products', index)
     app.get('/products/:id', show)
+    app.post('/products', verifyAuthToken, create)
 }
 export default productRoutes
