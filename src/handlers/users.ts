544ca214b4e2cc03dbe62b45f52e10262855d3b7
ozggnr <ozgecaylioglu@gmail.com>
2022-03-31 23:58:30 -0400

    added user authentication

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,30 +1,71 @@
 import express, { Request, Response } from "express"
 import { UserDB, User } from '../models/users-model'
+import jwt from 'jsonwebtoken'
 
 const users = new UserDB()
+const secret = process.env.TOKEN_SECRET || ''
 
 const index = async (req: Request, res: Response) => {
     const userList = await users.index()
     res.json(userList)
 }
+const show = async (req: Request, res: Response) => {
+    const user = await users.show(req.params.id)
+    res.json(user)
+}
 const create = async (req: Request, res: Response) => {
+    const user: User = {
+        id: req.body.id as Number,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+    }
     try {
-        const user: User = {
-            id: req.body.id as Number,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password,
-        }
         const newUser = await users.create(user)
-        res.json(newUser)
+        //Created new token for the user 
+        var token = jwt.sign({user: newUser}, secret)
+        res.json(token)
     } catch (error) {
-        res.send('New user can not be added')
-
+        res.status(400)
+        res.send(`New user can not be added ${error}`)
+    }
+}
+//To check if user give the correct credentials
+const auth = async (req: Request, res: Response) => {
+    const user: User = {
+        id: req.body.id as Number,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+    }
+    try {
+        //Send the credentials to the model to check credentials if everything is ok user will return
+        const authUser = await users.auth(user.email, user.password)
+        //Create new token for the user and send it to the client
+        var token = jwt.sign({user: authUser}, secret)
+        res.json(token)
+    } catch (error) {
+        res.status(401)
+        res.send(`New user can not be added ${error}`)
+    }
+}
+const verifyAuthToken = async (req: Request, res: Response, next: Function) => {
+    try {
+        const authHeader = req.headers.authorization || ''
+        const token = authHeader.split(' ')[1]
+        jwt.verify(token, secret)
+        next()
+    } catch (error) {
+        res.status(401)
+        res.send(`Invalid token`)
     }
 }
 const userRoutes = (app: express.Application) => {
-    app.get('/users', index)
+    app.get('/users', verifyAuthToken, index)
+    app.get('/users/:id', verifyAuthToken, show)
     app.post('/users', create)
+    app.post('/users/auth', auth)
 }
 export default userRoutes
--- src/models/users-model.ts
+++ src/models/users-model.ts
@@ -23,6 +23,17 @@ export class UserDB {
             throw new Error(`Ups! There is something wrong ${error}`)
         }
     }
+    async show(id: string): Promise<User> {
+        try {
+            const connection = await client.connect()
+            const query = 'SELECT * FROM users WHERE id = ($1)'
+            const result = await connection.query(query, [id])
+            connection.release()
+            return result.rows[0]
+        } catch(error) {
+            throw new Error(`Cannot find the user ${error}`)
+        }
+    }
     async create(user: User): Promise<User> {
         try {
             const connection = await client.connect()
@@ -34,5 +45,16 @@ export class UserDB {
         } catch(error) {
             throw new Error(`Cannot create new user ${error}`)
         }
+    }
+    //Check the users credentials when they sign in
+    async auth(email: string, password: string): Promise<User | null> {
+        const connection = await client.connect()
+        const query = 'SELECT email, password FROM users WHERE email=($1)'
+        const result = await connection.query(query, [email])
+        if (result.rows.length > 0) {
+            const user = result.rows[0]
+            if (bcrypt.compareSync(password + pepper, user.password)) return user
+        }
+        return null
     }
 }
