29472694cb9af1c70af1e032a2a2c0d1349ab79c
ozggnr <ozgecaylioglu@gmail.com>
2022-04-08 19:13:14 -0400

    added test case

--- src/database.ts
+++ src/database.ts
@@ -8,14 +8,26 @@ const {
     POSTGRES_HOST,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
-    POSTGRES_DB
+    POSTGRES_DB,
+    POSTGRES_DB_TEST,
+    ENV
 } = process.env
-
+let client: Pool = new Pool()
 //connection to database
-const client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
-})
+if (ENV === 'dev') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    })
+}
+if (ENV === 'test') {
+    client = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB_TEST,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    })
+}
 export default client
